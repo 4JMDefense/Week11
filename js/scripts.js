b6bbdb5cc08ca40e4729158486c93b8bf4ebc3b4
John Miguel Dizon <jdizon11@my.bcit.ca>
2023-03-22 12:35:37 -0700

    added time to function

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,16 @@
-const fname = 'bob'
+let fname = prompt("Name:")
+let ctime = prompt("Time:")
 
-function greet(name){
-    console.log("HELLO " + name)
+function greet(name, time = "Morning"){
+    console.log(`Good ${time} ${name}!!`)
 }
 
-greet(fname)
+greet(fname,ctime)
+
+// let username = prompt("Enter your name")
+
+// let greet = (name) => {
+//     console.log(`HI! ${name}`)
+// }
+
+// greet(username);
